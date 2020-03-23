79c1a2c5cf94b7da52b0ee3f49bcc1a60ebac25e
Andrew Jewett <jewett.aij@gmail.com>
2020-03-23 15:45:13 -0700

    added source code and README.md

---
+++ README.md
@@ -0,0 +1,189 @@
+[![GitHub repo size](https://img.shields.io/github/repo-size/jewettaij/adjacency_ubin)]()
+[![License](https://img.shields.io/badge/License-GPL2-green.svg)]()
+
+adjacency_ubin
+===========
+Generate a (sparse) adjacency matrix for a point cloud.  This program
+generates a list of all pairs of points that lie within a user-specified
+cutoff distance. This program uses a simple uniform binning algorithm.
+
+## Typical Usage:
+
+```
+adjacency_ubuin -r rmax [optional arguments..] < coordinate_file > matrix_file
+```
+
+This simple program reads a file containing X Y Z coordinates of a list of
+particles and prints out a list indicating which pairs of particles who are
+nearby (ie. within a distance of "rmax").
+
+Uniform binning (the simplest possible algorithm) is used to reduce
+the running time from *O(n^2) to *O(n+nx\*ny\*nz)*
+(where *n* is the number of points,
+and *nx*, *ny*, *nz* are the number of bins in the x,y,z directions).
+(This can optionally be controlled using the "-bins" argument.)
+This crude program is not well suited for highly non-uniform distributions
+of points, or point clouds in high dimensions.  For these applications,
+use a more sophisticated spatial subdivision algorithm.
+
+## Input format:
+The file containing the coordinates of the points in the point cloud
+should be a simple 3-column text file.
+```
+x1 y1 z1
+x2 y2 z2
+x3 y3 z3
+x4 y4 z4
+:  :  :
+xn yn zn
+```
+
+## Output format:
+All pairs of points which are in contact are reported using
+the following format.
+```
+i1 j1 1
+i2 j2 1
+i3 j3 1
+:  :  :
+```
+Here "i" and "j" refer to the ID numbers of two different particles
+that are nearby each other (separated by a distance of less than "rmax").
+*(The 3rd column ("1") is useful when computing contact frequency
+  from multiple different point clouds.  See below.)
+
+### Multiple point clouds (trajectories"):
+
+This list of coordinates can be optionally divided into multiple groups or 
+"snapshots", which can be thought of as frames from a trajectory.
+This file can read in RAW (simple 3-colum ASCII, shown above) or XYZ format.
+This program generates a sparse matrix indicating which pairs of particles
+are within a certain distance (rmax) relative to each other.
+When multiple snapshots are present, it will keep track of how
+frequently those particles were nearby eachother.
+
+
+### Input format (.RAW file format):
+
+The coordinate file can be divided into multiple groups or snapshots
+These are separated/delimited by blank lines (or lines with only comments).
+
+```
+# 1st snapshot:
+x11 y11 z11
+x12 y12 z12
+x13 y13 z13
+ :  :  :
+x1n y1n z1n
+
+# 2nd snapshot:
+x21 y21 z21
+x22 y22 z22
+x23 y23 z23
+ :  :  :
+x2n y2n z2n
+
+# 3rd snapshot:
+x31 y31 z31
+x32 y32 z32
+x33 y33 z33
+ :  :  :
+x3n y3n z3n
+
+ :  :   :
+```
+Each non-blank line of this file contains 3 numbers which are the coordinates
+of a particle. That particle is assigned an ID number equal to the number of 
+lines read since the beginning of the current snapshot (starting at 1). 
+
+### Output format for trajectories:
+```
+i1 j1 count1
+i2 j2 count2
+i3 j3 count3
+ :  :   :
+```
+Here "i" and "j" refer to the ID numbers of two different particles
+and "count" refers to the number of times (over all the snapshots) that 
+those two particles were found to be nearby each other
+(ie, found to be separated by a distance of less than "rmax").
+("count" is 1 if the coordinate file contains only a single snapshot.
+ Pairs "i" and "j" which are never in contact (count=0) are not reported.
+ Pairs where i > j are redundant and are not reported.)
+
+## Optional arguments:
+```
+  -r rmax      <- specify the threshold contact distance (1.5 by default).
+
+  -raw         <-The default input file format: 3-column space-
+                 delimited text file with blank lines separating frames.
+                 (RAW format is assumed by default.)
+  -xyz         <-Instead, assume the input coordinate file uses ".xyz" format.
+                 (This feature is not well tested.  Hopefully it works.)
+  -bins N      <-Specify the number of bins
+                 Alternatley, you can specify 3 numbers (nx ny nz)
+                 if you want it to vary in different directions.
+                 Binning is only used to speed up the computation, so
+                 these numbers should not effect the result, as long as 
+                       (xmax-xmin)/nx >= rmax
+                       (ymax-ymin)/ny >= rmax
+                       (zmax-zmin)/nz >= rmax
+                 where xmax, xmin, ymax, ymin, zmax, zmin are the maximum and
+                 minimum coordinates in the input file.
+                 However using a small number of bins may reduce memory usage.
+                 (NOTE: The "-bins" option has not been tested and may not work)
+```
+
+
+## Changing the number of dimensions:
+
+*Note:* This program can be compiled to work with coordinate files for particles
+in dimensions other than 3 by changing the compiler flags at compile time
+from *"-DG_DIM=3"* to *"-DG_DIM=4"*. (The number of dimensions is 3 by default.)
+*(One way to do this editing the *"setup_gcc.sh"* file.  See below.)*
+
+This program works well for 2D and 3D point clouds.
+However this simple program is *particularly ill-suited* for high
+dimensional point clouds.  Uniform binning requires exponentially
+more space and time as the number of dimensions increases.
+
+
+## Compilation
+
+### Linux and Apple macOS:
+
+```
+cd src
+source setup_gcc.sh
+make
+```
+
+*(Note:  If you are not using the bash shell,
+enter "bash" into the terminal beforehand.)*
+
+*(Note: Apple users will need to install the gcc compiler
+and other build tools using Xcode or brew.)*
+
+*(Note: If you receive an error regarding "omp" or "OpenMP", then use
+"setup_gcc_serial.sh" instead.  This may be necessary for apple users.)*
+
+### Windows 10:
+
+Install HyperV (with linux), or the Windows Subsystem for Linux (WSL) and run
+
+```
+sudo apt-get install build-essential
+```
+and then follow the instructions above.
+(Older windows users can install Cygwin or MinGW, or linux via virtualbox.)
+
+
+## Development Status: *alpha*
+
+This program was quickly written has only tested on a few point clouds.
+Some of the arguments documented above may not yet work.
+
+
+## License
+
+*adjacency_ubin* is available under the terms of the [GPL-2.0 license](LICENSE.txt).
---
+++ src/adjacency_ubin.cpp
@@ -0,0 +1,403 @@
+/// @file     adjacency_ubin.cpp
+/// @brief    Generate a (sparse) adjacency matrix for a point cloud which
+///           lists all pairs of points lie within a cutoff distance.
+///           This program uses a simple uniform binning algorithm.
+/// @brief    Generate a (sparse) adjacency matrix for a point cloud.
+///           This program generates a list of all pairs of points
+///           that lie within a user-specified cutoff distance.
+///           This program uses a simple uniform binning algorithm.
+/// @author   Andrew Jewett
+/// @license  GPL-2.0
+///
+/// @note  To change the number of dimensions in which the point cloud lives,
+/// (ie, the number of coordinates on each line of the structure file)
+/// then compile using following compiler flag "-DG_DIM=4" (4 dimensions).
+/// (You can also edit the "global_defs.h" file.)
+
+
+#include <cassert> //needed for assert()
+#include <cstring> //needed for strcmp()
+#include <cstdlib> //needed for atol()
+#include <map>
+#include <vector>
+using namespace std;
+
+
+#include "global_defs.h"
+#include "bins.h"
+#include "io.h"
+
+
+// global variables
+const char g_program_name[]   = "adjacency_ubin";
+const char g_version_string[] = "0.3.1";
+const char g_date_string[]    = "<2020-3-23>";
+
+
+
+
+struct Settings
+{
+
+  enum CoordFileFormat {COORD_FORMAT_RAW,
+                        COORD_FORMAT_XYZ};
+
+  CoordFileFormat coord_file_format;
+  Real rcut; //cutoff distance for whether two atoms are "in contact"
+  Icrd aUserNumBins[g_dim]; //let the user choose the number of bins
+
+  Settings() {
+    coord_file_format = COORD_FORMAT_RAW;
+    rcut = 1.5;
+    for (int d = 0; d < g_dim; d++)
+      aUserNumBins[d] = UNINITIALIZED;
+  }
+
+}; //struct Settings
+
+
+
+void PrintAdjacency(const map<pair<Iatm,Iatm>,long>& m,
+                    ostream& out_file)
+{
+  for(map<pair<Iatm,Iatm>,long>::const_iterator p=m.begin(); p!=m.end(); p++)
+  {
+    pair<Iatm,Iatm> ijpair = p->first;
+    Iatm ia = ijpair.first;
+    Iatm ja = ijpair.second;
+    long count = p->second;
+    out_file << ia+1 << " " << ja+1 << " " << count << "\n";
+  }
+}
+
+
+
+
+
+void 
+CalcAdjacency(map<pair<Iatm,Iatm>,long>& m,
+              ConstVect *aaX,  // <--> equivalent to "double (*x)[g_dim],"
+              Iatm num_atoms,
+              Real rcut,
+              Bins &bins)
+{
+  Icrd aIcrd[g_dim];
+  Icrd aJcrd[g_dim];
+  Real rcutsq = rcut*rcut;
+
+  for (Iatm ia = 0;  ia < num_atoms;  ia++)
+  {
+    Ibin ib = bins.aBinFromIatm[ia]; //the bin-id of bin containing atom ia
+    // Find the array of coordinates (aIcrd) for that bin (ib):
+    bins.IcrdFromIbin(aIcrd, ib);
+
+    aJcrd[0] = ICRD_UNINITIALIZED; // <-signal this is the first neighbor bin.
+    while (bins.NextNeighborBin(aIcrd, aJcrd))
+    {
+      // loop over all of the atoms in that bin
+      Ibin jb  = bins.IbinFromIcrd(aJcrd);
+      // original version:
+      //Iatm ja = bins.aHeadFromIbin[jb]; // "ja" is the atom-id of a neighbor
+      //                                  // candidate located in bin "jb"
+      // memory efficient version:
+      Ibin jbb = jb % bins.num_bins;
+      Iatm ja  = bins.aHeadFromIbin[jbb];
+
+      while (ja >= 0)
+      {
+        if (DistanceSqd(aaX[ia], aaX[ja]) < rcutsq)
+          m[ pair<Iatm,Iatm>(ia, ja) ] = 1;
+        ja = bins.aNextFromIatm[ja];
+      }
+    }
+  } //for (Iatm ia = 0;  ia < num_atoms;  ia++)
+} //CalcAdjacency()
+
+
+
+# if 0
+// COMMENTING OUT
+void 
+CalcAdjacency(map<pair<Iatm,Iatm>,long>& m,
+              ConstVect *aaX,  // <--> equivalent to "double (*x)[g_dim],"
+              Iatm num_atoms,
+              Real rcut,
+              Bins &bins)
+{
+  Icrd aIcrd[g_dim];
+  Icrd aJcrd[g_dim];
+  for (Iatm ia = 0;  ia < num_atoms;  ia++)
+  {
+    Ibin ib = bins.aBinFromIatm[ia]; //the bin-id of bin containing atom ia
+    //   Note: This is equivalent to the next two commented lines:
+    //  IcrdFromX(aXX[ia], aIcrd);
+    //  Ibin ib = Bins.IbinFromX(aax[ia]);
+
+    Ibin jb=UNINITIALIZED; //jb is bin-id of one of the nearby bins
+    while ((jb = bins.NextNeighborBin(ib, jb)) >= 0)
+    {
+      // loop over all of the atoms in that bin
+      Iatm ja = bins.aHeadFromIbin[jb]; //ja is the atom-id an atom in bin jb
+      while (ja >= 0)
+      {
+        if (DistanceSqd(aaX[ia], aaX[ja]) < rcut*rcut)
+          m[ pair<Iatm,Iatm>(ia, ja) ] = 1;
+        ja = bins.aNextFromIatm[ja];
+      }
+    }
+  } //for (Iatm ia = 0;  ia < num_atoms;  ia++)
+} //CalcAdjacency()
+#endif //#if 0
+
+
+
+void 
+ParseArgs(int argc,
+          char **argv,
+          Settings &settings)
+{
+
+  stringstream explanation;
+  explanation 
+    <<"\n"
+    "Explanation:\n"
+    "\n"
+    "  This program reads a coordinate file containing one (or more) structures\n"
+    "  (in .raw or .xyz format), and generates a sparse matrix of contacts\n"
+    "  i1 j2 count1\n"
+    "  i2 j2 count2\n"
+    "  i3 j3 count3\n"
+    "   :  :   :\n"
+    "  where \"i\" and \"j\" refer to monomers in the structure(s), and\n"
+    "  \"count\" refers to the number of times those two monomers were found\n"
+    "  to be in contact with eachother in this structure(s).\n"
+    "    (\"in contact\" <--> to be spatially separated by a distance <= dthresh)\n"
+    "\n"
+    "Typical Usage:\n"
+    "\n"
+    "\n"
+    <<"  "<< g_program_name<<" -d dthresh [optional arguments..] < coordinate_file > matrix_file\n"
+    "\n"
+    "\n"
+    "Optional arguments:\n"
+    "\n"
+    "  -d dthresh   <- specify the threshold contact distance (1.5 by default).\n"
+    "\n"
+    "  -raw         <-The default input file format: 3-column space-\n"
+    "                 delimited text file with blank lines separating frames.\n"
+    "                 Note: \".RAW\" format is assumed by default.\n"
+    "  -xyz         <-Instead, assume the input coordinate file uses \".XYZ\" format.\n"
+    "                 (This feature is not well tested.  Hopefully it works.)\n"
+    "  -bins N      <-Specify the number of bins (aka voxels) in each direction.\n"
+    "                 Alternatley, you can specify 3 numbers (in 3 dimensions)\n"
+    "                 if you want it to vary in different directions.\n"
+    "                 (This option has not been tested and may not work. 2013-60-6)\n"
+    "\n";
+
+  try {
+    int which_arg = 1;
+    while (which_arg < argc)
+    {
+      if (strcmp(argv[which_arg], "-raw") == 0)
+      {
+        settings.coord_file_format = Settings::COORD_FORMAT_RAW;
+        which_arg += 1;
+      }
+      else if (strcmp(argv[which_arg], "-xyz") == 0)
+      {
+        settings.coord_file_format = Settings::COORD_FORMAT_XYZ;
+        which_arg += 1;
+      }
+      else if ((strcmp(argv[which_arg], "-r") == 0) ||
+	       (strcmp(argv[which_arg], "-d") == 0) ||
+	       (strcmp(argv[which_arg], "-dthresh") == 0))
+      {
+        if (which_arg+1 == argc) { 
+          stringstream errmsg;
+          errmsg << "\nError: expected a number following \""<<argv[which_arg]
+                 << "\"\n";
+          throw ArgParseErr(errmsg.str());
+        }
+        settings.rcut = atof(argv[which_arg+1]);
+        which_arg += 2;
+      }
+      else if (strcmp(argv[which_arg], "-bins") == 0)
+      {
+        if (which_arg+1 == argc) { 
+          stringstream errmsg;
+          errmsg << "\nError: expected at least one number following \""<<argv[which_arg]
+                 << "\"\n";
+          throw ArgParseErr(errmsg.str());
+        }
+
+        vector<Icrd> vUserNumBins; //<-Store numbers here while reading arg list
+        int j = which_arg+1;
+        while ((j < argc) && 
+               (strlen(argv[j]) != 0) &&
+               isdigit(argv[j][0]))
+        {
+          vUserNumBins.push_back(atoi(argv[j]));
+          j++;
+        }
+        // Then copy the numbers into the settings.aUserNumBins[] array:
+        if (vUserNumBins.size() == 1)
+          // That means use the same number of bins in all 3 directions
+          for (int d = 0; d < g_dim; ++d) 
+            settings.aUserNumBins[d] = vUserNumBins[0];
+        else if (vUserNumBins.size() == g_dim) {
+          for (int d = 0; d < g_dim; ++d) 
+            settings.aUserNumBins[d] = vUserNumBins[d];
+        }
+        else {
+          stringstream errmsg;
+          errmsg << "Error: Wrong number of integers ("
+                 << vUserNumBins.size()
+                 << ")\n"
+                 << "       following \""<<argv[which_arg]<<"\" argument.\n"
+                 << "       Expected either 1 or "<<g_dim<<" integers.\n";
+          throw ArgParseErr(errmsg.str());
+        }
+        which_arg = j;
+      }
+      else
+      {
+        stringstream errmsg;
+        errmsg << "\nError: Unrecognized argument: \""<<argv[which_arg]<<"\"\n";
+        throw ArgParseErr(errmsg.str());
+      }
+    } //while (which_arg < argc)
+  }
+  catch (ArgParseErr& e)
+  {
+    cerr << "--------------------------------------------------\n";
+    cerr << "--------------------------------------------------\n";
+    cerr << "       Error in argument list: \n" << e.what() << endl;
+    cerr << "--------------------------------------------------\n";
+    cerr << "--------------------------------------------------\n";
+    cerr << "\n";
+    cerr << explanation.str() << endl;
+    exit(-1);
+  }
+
+  //---- finished parsing the argument list ----
+
+} //ParseArgs()
+
+
+
+
+
+
+int
+main(int argc, char **argv)
+{
+
+  //---- Load the coordinate file into a large buffer ----
+
+  try 
+  {
+    long long line_count = 1; //counts all lines including blanks and comments
+
+    map<pair<Iatm,Iatm>,long> m_tot;
+
+    cerr << g_program_name   << ", v"
+         << g_version_string << " "
+         << g_date_string    << "\n";
+
+    // Process the argument list
+    Settings settings;
+    ParseArgs(argc, argv, settings);
+
+    cerr << "  reading input file...\n";
+
+    long long frame_counter = 0;
+
+    // Read one structure from the file and calculate its adjacency matrix.
+    while(cin)
+    {
+      Vect *aaX = NULL;
+
+      // Read in the next frame
+      long num_atoms = UNINITIALIZED; //number of atoms in each snapshot/frame
+
+      if (settings.coord_file_format == Settings::COORD_FORMAT_RAW)
+        num_atoms = ReadCoordsRAW(cin, &aaX, line_count);
+      else if (settings.coord_file_format == Settings::COORD_FORMAT_XYZ)
+        num_atoms = ReadCoordsXYZ(cin, &aaX, line_count);
+      else
+        assert(0);
+
+      // We could be at a point in the file with trailing whitespace.  Check
+      // to make sure that we did actually read something before we continue.
+
+      if (num_atoms > 0) {
+
+        Bins bins(aaX, 
+                  num_atoms, 
+                  settings.rcut,
+                  settings.aUserNumBins);
+
+        map<pair<Iatm,Iatm>,long> m; // m = adjacency matrix (sparse)
+
+        //    Note: If you are unfamilliar with C++ "maps" or "pairs", 
+        //          they are similar to python dictionaries and tuples.  See:
+        //    http://www.cplusplus.com/reference/map/map/
+        //    http://www.cplusplus.com/reference/utility/pair/
+
+        CalcAdjacency(m,
+                      aaX,
+                      num_atoms,
+                      settings.rcut,
+                      bins);
+
+
+	// --- COMMENTING OUT: making a separate matrix for each structure 
+	// ---              is slow and uses a lot of disk space.          
+        //Now print the adjacency matrix
+        //stringstream fname;
+        //fname << "out_adjacency_" << (frame_counter+1) << ".dat";
+        //fstream out_file(fname.str().c_str(), ofstream::out);
+        //PrintAdjacency(m, out_file);
+        //out_file.close();
+        // ---
+
+
+        // Add this adjacency matrix to the total adjacency matrix.
+        for(map<pair<Iatm,Iatm>,long>::const_iterator p = m.begin();
+            p != m.end();
+            p++)
+        {
+          pair<Iatm,Iatm> ijpair = p->first;
+          long count = p->second;
+          if (m_tot.find(ijpair) != m_tot.end())
+            m_tot[ijpair] += count;
+          else
+            m_tot[ijpair] = count;
+        }
+
+        frame_counter++;
+        //cerr << " done" << endl;
+        cerr << "    finished processing frame " << frame_counter << endl;
+      }
+      else
+        assert(! cin);
+
+      delete [] aaX;
+
+    } //while(cin)
+
+
+    if (frame_counter > 0) {
+      // Now print the total adjacency matrix
+      PrintAdjacency(m_tot, cout);
+    }
+
+  } //try
+  catch (InputErr& e)
+  {
+    cerr << "       Error in input file(s): \n" << e.what() << endl;
+    exit(-1);
+  }
+
+} //main()
+
